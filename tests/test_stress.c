e7a2abbae96a7bab15e295ac5032149e9375a6ed
Ritvik Sharma <contact4ritvik@gmail.com>
2025-12-07 04:08:48 -0800

    Updated stress test approach.

--- tests/test_stress.c
+++ tests/test_stress.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "memoryallocator.h"
+#include "memory_allocator.h"
 
-#define N 5000
+#define N 10
 
 int main() {
     printf("Stress testing allocator...\n");
@@ -12,14 +12,13 @@ int main() {
     srand(1234);
 
     // random alloc/free
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < 10; i++) {
         size_t s = rand() % 2048 + 1;
         ptrs[i] = memalloc(s);
-
         if (!ptrs[i]) {
             return printf("FAIL: memalloc returned NULL\n"), 1;
         }
-
+        printf("Alloc %d: %p, size %zu\n", i, ptrs[i], s);
         if (rand() % 3 == 0) {
             memfree(ptrs[i]);
             ptrs[i] = NULL;
